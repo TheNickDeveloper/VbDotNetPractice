09eaa436789ac6505f330a8a3daf28f14dd8564d
Pochien Tsai <nick921209@gmail.com>
2019-06-12 18:30:54 +0800

    practice update
    1.  general class practice
    2.  generic type practice
    3.  reflaction practice

---
+++ VbPractice/VbPractice/BasicInfo.vb
@@ -0,0 +1,5 @@
+﻿Public Class BasicInfo
+    Public Property ID() As String
+    Public Property Name() As String
+    Public Property Gender() As String
+End Class
--- VbPractice/VbPractice/Module1.vb
+++ VbPractice/VbPractice/Module1.vb
@@ -1,6 +1,6 @@
 ﻿Module Module1
 
-    Sub Main()
+    Public Sub Main()
 
         Dim areaCalculator As New Reectangle(10, 2)
         areaCalculator.Length = 10
@@ -8,9 +8,27 @@
 
         Dim area As Double = areaCalculator.CalculateArea()
 
+        Dim k = GetBasicInfoList(Of BasicInfo)()
         Console.WriteLine($"area is {area.ToString()}")
         Console.ReadLine()
 
     End Sub
 
+    Private Function GetBasicInfoList(Of T)() As List(Of T)
+        Dim resultList As New List(Of T)
+        Dim type = GetType(T)
+        Dim properties = type.GetProperties()
+
+        For index = 1 To 10
+            Dim ob = Activator.CreateInstance(Of T)
+
+            For Each prop In properties
+                prop.SetValue(ob, index.ToString())
+            Next
+
+            resultList.Add(ob)
+        Next
+
+        Return resultList
+    End Function
 End Module
